1660798fcb0c651b73891c2a9d2f044b48745613
kevinjom <gokevinvan@gmail.com>
2016-09-25 20:00:13 +0800

    A simple servlet which just responds `hello xxx`.

---
+++ src/main/java/com/github/kevinjom/servleting/InboxRepository.java
@@ -0,0 +1,10 @@
+package com.github.kevinjom.servleting;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InboxRepository {
+    public List<String> getAll(String userId) {
+        return Arrays.asList("welcome to inbox");
+    }
+}
---
+++ src/main/java/com/github/kevinjom/servleting/InboxServlet.java
@@ -0,0 +1,16 @@
+package com.github.kevinjom.servleting;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class InboxServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("hello sevleting");
+    }
+}
+
